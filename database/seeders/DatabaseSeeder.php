fb86c053173876e58ffd009a8aeecd2684605e53
neeraj <help.pagpriya@gmail.com>
2025-11-22 01:01:22 +0530

    AppServiceProvider.php update, database seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'pagpriya',
+            'email' => 'help.pagpriya@gmail.com',
+            'password' => hash('admin123456')
         ]);
     }
 }
