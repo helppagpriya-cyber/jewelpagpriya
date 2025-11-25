1dae5792f473d59591f0c1fc7981222ef82899be
helppagpriya-cyber <help.pagpriya@gmail.com>
2025-11-25 16:40:24 +0530

    old slider remove

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,29 +12,7 @@
     @endif
 <livewire:hero-slider/>
 
-{{-- <div class="slideshow-container h-[450px] overflow-hidden">
-     <!-- Full-width images with number and caption text -->
-  @foreach($sliders as $slider)
-  <div class="mySlides fade">
-    <div class="numbertext hidden">{{ $slider->id }}</div>
-    <a wire:navigate href="{{ url('/all-products') }}">
-    <img src="{{ asset('storage/' . $slider->image) }}"  ></a>
-    <div class="text">{{ $slider->text }}</div>
-  </div>
-  @endforeach
-  <!-- Next and previous buttons -->
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-</div>
-<br>
 
-<!-- The dots/circles -->
-
-<div style="text-align:center">
-  @foreach($sliders as $slider)
-    <span class="dot" onclick="currentSlide{{ $slider->id }}"></span>
-  @endforeach
-</div> --}}
 
 {{-- static data --}}
 <div class="container mx-auto">
@@ -165,35 +143,4 @@
 
 
 
-<script>
-    let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-</script>
 @endsection
